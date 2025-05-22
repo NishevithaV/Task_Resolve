00d79eb96cf79bdf85d395098fe154fdb45e5c74
Nishevitha <nishevitha.v@gmail.com>
2025-05-21 23:33:03 -0400

    code refactoring - decorators & signals for user profile clicking introduced

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, input, computed, EventEmitter, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,16 +8,23 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   // settable from outside
-  // exclamation mark means value of property will be set from outside 
-  // allows to bind data from parent component
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // exclamation mark means value of property will be set from outside - allows to bind data from parent component
+  @Input ({required: true}) id! : string;
+  @Input({required: true}) avatar!: string;
+  // required (configuration) to enable error checking
+  @Input({required: true}) name!: string;
 
-  get imagePath() {
-    return `assets/users/` + this.avatar;
-  }
+  // avatar = input.required<string>(); // avatar property is an input to this component
+  // name = input.required<string>();
+
+  @Output() select = new EventEmitter();
+
+  // only computes when avatar changes - improves efficiency
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar;
+  });
 
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
 }
